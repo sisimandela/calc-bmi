897630a20187241bbee3d67b41b4659b2ace1989
Sisay Mandefro <sisimandela19@gmail.com>
2020-06-28 09:10:53 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+#what is this package?
+ Get the acurate body mass index
+
+#installation
+`npm i bmi-calc --save`
+
+```
+import { bmi-calc } from 'bmi-calc'
+
+var bmi = require('bmi-calc');
+
+// 1.8Meter Height, 75KG Weight
+console.log(bmi.bmi(1.8, 75));
+
+// { bmi: '23.15', index: 'Normal Weight' }
+```
+#Author
+Sisay Mandefro
---
+++ index.js
@@ -0,0 +1,32 @@
+var bmi = (height, weight) =>{
+
+    bmi = (weight/(height*height)).toFixed(2);
+
+    if(bmi<18.5)
+    {
+    index = "Under Weight";
+    }
+    else if (bmi>=18.5 && bmi<=24.9)
+    {
+    index = "Normal Weight";
+    }
+
+    else if (bmi>24.9 &&  bmi<=29.9)
+    {
+        index = "over Weight";
+    }
+    else
+    {
+     index = "obesity";
+    }
+
+
+    return {
+        bmi: bmi,
+        index: index,
+    }
+
+};
+
+
+module.exports.bmi = bmi;
